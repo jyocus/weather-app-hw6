c933fa374608a24cca7fe167bd7a5e604cf67bff
Justin Yocus <justin.o.yocus@gmail.com>
2020-10-07 18:23:44 -0500

    HTML skeleton and corrected API call in script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//wait until the DOM is ready to load
 $(document).ready(function() {
-    
+    //empty variable that will be populated by the user input for the city they select
     var cityname = "";
-    var queryURL = "https://cors-anywhere.herokuapp.com/" + "api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid=" + "{c2716f16e2b49adc65224dba816571c2}"
+    var queryURL = "https://cors-anywhere.herokuapp.com/" + "api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + "{c2716f16e2b49adc65224dba816571c2}"
+
 })

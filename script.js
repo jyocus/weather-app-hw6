17a74e8d38a80bb1be90a619cad9040a90acae52
Justin Yocus <justin.o.yocus@gmail.com>
2020-10-07 17:44:32 -0500

    api and key added to script, links to css/foundation/script page to html

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    
+    var cityname = "";
+    var queryURL = "https://cors-anywhere.herokuapp.com/" + "api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid=" + "{c2716f16e2b49adc65224dba816571c2}"
+})

5cb12ec391bccbfcd3065949378846be3ac0fda6
Justin Yocus <justin.o.yocus@gmail.com>
2020-10-07 19:20:02 -0500

    added click event and ajax call to button for test

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 //wait until the DOM is ready to load
 $(document).ready(function() {
     //empty variable that will be populated by the user input for the city they select
-    var cityname = "";
-    var queryURL = "https://cors-anywhere.herokuapp.com/" + "api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + "{c2716f16e2b49adc65224dba816571c2}"
+   var cityname; 
+   
+    $('#submitSearch').click(function() {
+        // console.log(Working);
+        cityname = "Chicago";
 
+        var queryURL = "https://cors-anywhere.herokuapp.com/" + "api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=c2716f16e2b49adc65224dba816571c2";
 
-})
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+
+          .then(function(response) {
+              console.log(response);
+          });
+    });
+
+});
